993625ed06c89561ed5bbb420e9265a96ab5cae2
Patrick <patricktan9955@hotmail.com>
2021-06-13 05:20:40 +0800

    Redesigned data model and partial implementation of queries.
    1) Redesigned data model to support multiple uploads when talking to the bot in private message
    2) Implemented a clear function that deletes all files that user uploads. Works in both PM and group chat.
    3) Partial implementation of UI (ReplyKeyboardMarkup) for querying of bot done.

--- main_whenmeetbot.py
+++ main_whenmeetbot.py
@@ -21,18 +21,27 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 def start(update, context):
-    welcome = '''Welcome to WhenMeetBot!\nFor help, type '/help'.\nTo upload your .ics file, type '/upload'.\nTo query for common free times, type '/find'.'''
+    welcome = '''Ohayo minasan! Welcome to WhenMeetBot!\nFor help/tasukete, type '/help'.\nTo upload your .ics file, type '/upload'.\nTo query for common free times, type '/find'.'''
     context.bot.send_message(
         text=welcome,
         chat_id=update.message.chat_id
     )
 
 def help(update, context):
-    help = "To use the bot, add the bot to a group chat, and have all members use '/upload' to upload their .ics files!\n To query for common free times, use '/find' :D"
-    context.bot.send_message(
-        text=help,
-        chat_id=update.message.chat_id
-    )
+    chat_type = update.message.chat.type
+    help_group = "To use the bot, add bot-chan to a group chat, and have all members use '/upload' to upload their .ics files!\n To query for common free times, use '/find'\nTo clear your uploaded file, use '/clear'...baka :3"
+    help_private = "To use the bot, use /upload to upload the files that you want to compare.\nTo query for common free times, use '/find'.\nTo clear all uploaded files, use '/clear'...baka :3"
+    if chat_type == 'private':
+        context.bot.send_message(
+            text=help_private,
+            chat_id=update.message.chat_id
+        )
+
+    else:
+        context.bot.send_message(
+            text=help_group,
+            chat_id=update.message.chat_id
+        )
 
 def cancel_upload(update, context):
     text = 'upload iz kil'
@@ -42,17 +51,158 @@ def cancel_upload(update, context):
     )
     return ConversationHandler.END
 
-#TODO
+def clear(update, context):
+    chat_type = update.message.chat.type
+    if chat_type == 'private':
+        user_id = update.message.chat_id
+
+        db.child('private').child(user_id).remove()
+
+    else:
+        group_id = update.message.chat_id
+        user_id = update.message.from_user.id
+
+        db.child('group').child(group_id).child(user_id).remove()
+
+    remove_text = "Y-y-you removed your data from bot-chan's storage?! I-it's not like I care...baka"
+    context.bot.send_message(
+        text=remove_text,
+        chat_id=group_id
+    )
+
 def find(update, context):
-    pass
+    # Check if query done from group or PM with bot
+    chat_type = update.message.chat.type
+    context.chat_data['included'] = []
+
+    # Query from group chat
+    if chat_type == 'group' or chat_type == 'supergroup':
+        group_id = update.message.chat_id
+
+        # Get from db a list of all userIDs for the group
+        raw_user_ids = db.child('group').child(group_id).get().val()
+
+        # Check if user_ids is empty
+        if not raw_user_ids:
+            prompt_upload = "OwO bot-chan couldn't find data :( Use '/upload' first uwu....b-b-baka"
+            context.bot.send_message(
+                text=prompt_upload,
+                chat_id = group_id
+            )
+            return ConversationHandler.END
+
+        user_ids = list(raw_user_ids.keys())
+
+        # Populate keyboard buttons with names of users
+        keyboard = []
+        rowcnt = 0
+        row = []
+        for userid in user_ids:
+            username = update.message.chat.get_member(userid).user.username
+            fullname = update.message.chat.get_member(userid).user.full_name
+            final_name = "{} ({})".format(fullname, username)
+
+            context.chat_data[final_name] = userid
+            
+            if rowcnt < 3:
+                row.append(final_name)
+                rowcnt += 1
+            else:
+                rowcnt = 0
+                keyboard.append(copy.copy(row))
+                row = []
+            
+        if len(row) != 0:
+            keyboard.append(copy.copy(row))
+        keyboard.append(['Done', 'Cancel'])
+
+        prompt = "UwU you want to find common free times? Tell bot-chan who you want to include...baka >w<"
+        context.bot.send_message(
+            text=prompt,
+            chat_id=update.message.chat_id,
+            reply_markup=ReplyKeyboardMarkup(keyboard=keyboard)
+        )
+        return 1
+
+    # Query through PM with bot
+    elif chat_type == "private":
+        userid = update.message.chat_id
+
+        # Check if entry exists in db
+        data = db.child('private').child(userid).get().val()
+        
+        # If entry doesn't exist
+        if not data:
+            prompt_upload = "OwO bot-chan couldn't find data :( Use '/upload' first uwu....b-b-baka"
+            context.bot.send_message(
+                text=prompt_upload,
+                chat_id=userid
+            )
+            return ConversationHandler.END
+        
+        context.chat_data['included'].extend(data)
+
+        start_time_prompt = ">w< bot-chan found your ics file uwu plz gib bot-chan a start date to search from e.g. '2021-01-01' for 1 Jan 2021"
+        context.bot.send_message(
+            text=start_time_prompt,
+            chat_id=userid
+        )
+        return 2
+
+    # Query through channel
+    else:
+        context.bot.send_message(
+            text="bot-chan no worku with channels now...baka >w<",
+            chat_id=update.message.chat_id
+        )
+        return ConversationHandler.END
 
 #TODO
 def find_persons_to_query(update, context):
-    pass
+    user_selection = update.message.text
 
-#TODO
+    # # Not valid user input
+    # if not user_selection:
+    #     reprompt = "B-b-baka...nani kore is this bot-chan no understando try again plz"
+    #     context.bot.send_message(
+    #         text=reprompt,
+    #         chat_id=update.message.chat_id
+    #     )
+    #     return 1
+
+    # User done selecting users
+    if user_selection == 'Done':
+        prompt_time = "Oki uwu now plz gib bot-chan a start date to search from e.g. '2021-01-01' for 1 Jan 2021"
+        context.bot.send_message(
+            text=prompt_time,
+            chat_id=update.message.chat_id,
+            reply_markup=ReplyKeyboardRemove()
+        )
+        return 2
+
+    # Possibly valid user input i.e. string of form xxx(xxx)
+    # Check if username in saved chat_data
+    elif user_selection not in context.chat_data.keys():
+        reprompt = "B-b-baka...nani kore is this bot-chan no understando try again plz"
+        context.bot.send_message(
+            text=reprompt,
+            chat_id=update.message.chat_id
+        )
+        return 1
+
+    # Definitely valid user input
+    else:
+        # Retrieve user_id from username
+        user_id = context.chat_data.pop(user_selection)
+
 def cancel_find(update, context):
-    pass
+    cancel_text = "Bot-chan cancelled your request...d-d-don't get me wrong, it's not like I want you to use me again...baka"
+    context.bot.send_message(
+        text=cancel_text,
+        chat_id=update.message.chat_id,
+        reply_markup=ReplyKeyboardRemove() 
+    )
+    return ConversationHandler.END
 
 #TODO
 def find_start_time(update, context):
@@ -66,25 +216,62 @@ def find_end_time(update, context):
 def find_min_interval(update, context):
     pass
 
-def upload(update, context):
-    group = update.message.chat_id
-    user = update.message.from_user.id
+#TODO
+def display_result(update, context):
+    pass
 
-    # Check if ics file for this user already exists in db
-    if db.child(group).child(user).get().val() != None:
-        prompt = "B-b-baka, you have a .ics in me already owo...send 'yes' to overwrite or 'no' to cancel"
+#TODO
+def upload(update, context):
+    group_id = update.message.chat_id
+    user_id = update.message.from_user.id
+    chat_type = update.message.chat.type
+
+    if chat_type == "channel":
+        prompt = "Ara ara~ Channel wa dame desu yo"
         context.bot.send_message(
             text=prompt,
-            chat_id=group
+            chat_id=group_id
         )
-        return 2
 
-    upload_text = 'Plz send .ics files thenk or use "/cancel" to cancel'
-    context.bot.send_message(
-        text=upload_text,
-        chat_id=update.message.chat_id
-    )
-    return 1
+    is_PM = False if chat_type == "group" or chat_type == "supergroup" else True
+
+    # Case for PM
+    if is_PM:
+        # # Check if ics file for this user already exists in db
+        # if db.child('private').child(user_id).get().val() != None:
+        #     prompt = "B-b-baka, you have a .ics in me already owo...send 'yes' to overwrite or 'no' to cancel"
+        #     context.bot.send_message(
+        #         text=prompt,
+        #         chat_id=group_id
+        #     )
+        #     return 2
+
+        upload_text = "I-it's not like I need your .ics files b-b-baka! Upload your .ics files thenk or use '/cancel' to cancel"
+        context.bot.send_message(
+            text=upload_text,
+            chat_id=update.message.chat_id
+        )
+        return 1
+
+    # Case for group chat
+    else:
+        print(db.child('group').child(group_id).child(user_id).get().val())
+        # Check if ics file for this user already exists in db
+        if db.child('group').child(group_id).child(user_id).get().val() != None:
+            print('Prompt group overwrite')
+            prompt = "B-b-baka, you have a .ics in me already owo...send 'yes' to overwrite or 'no' to cancel"
+            context.bot.send_message(
+                text=prompt,
+                chat_id=group_id
+            )
+            return 2
+
+        upload_text = "I-it's not like I need your .ics files b-b-baka! Upload your .ics files thenk or use '/cancel' to cancel"
+        context.bot.send_message(
+            text=upload_text,
+            chat_id=update.message.chat_id
+        )
+        return 1
 
 def reprompt(update, context):
     reprompt_text = 'Plz no torture our bot owo send proper .ics file pls....baka >w<'
@@ -110,7 +297,7 @@ def ask_confirmation(update, context):
         )
         return ConversationHandler.END
 
-def not_understood(update, context):
+def upload_not_understood(update, context):
     text = "I no understando, zen zen wakaranai...baka...\nSend 'yes' to overwrite or 'no' to cancel"
     context.bot.send_message(
         text=text,
@@ -118,6 +305,19 @@ def not_understood(update, context):
     )
     return 2
 
+#TODO
+def start_not_understood(update, context):
+    pass
+
+#TODO
+def end_not_understood(update, context):
+    pass
+
+#TODO
+def interval_not_understood(update, context):
+    pass
+
+#TODO
 def on_doc_upload(update, context):
     # # Check if message sent from group. If not, prompt user to add bot to group
     # if update.message.chat.type != "group" and update.message.chat.type  != "supergroup":
@@ -128,10 +328,11 @@ def on_doc_upload(update, context):
     #     )
     #     return 1
     
-    group = update.message.chat_id
-    user = update.message.from_user.id
+    group_id = update.message.chat_id
+    user_id = update.message.from_user.id
     doc = update.message.document
     fileid = update.message.document.file_id
+    is_PM = True if update.message.chat.type == 'private' else False
     content = ""
 
     # Download the document and populate content string
@@ -143,20 +344,32 @@ def on_doc_upload(update, context):
             content += line
     os.remove('{}'.format(fileid))
 
-    # Write ics file to database if no entry for user present
-    if db.child(group).child(user).get().val() == None:
-        db.child(group).child(user).set(content)
-        # print('Added')
+    if is_PM:
+        current_len = db.child('private').child(user_id).get().val()
+        if not current_len:
+            current_len = 0
+        else:
+            current_len = len(current_len)
+        db.child('private').child(user_id).child(current_len).set(content)
+        print('Added private + {}'.format(current_len))
 
-    # Otherwise update the file if entry for user already present
     else:
-        db.child(group).update({user : content})
-        # print('Updated')
+        # Write ics file to database if no entry for user present
+        if db.child('group').child(group_id).child(user_id).get().val() == None:
+            db.child('group').child(group_id).child(user_id).set(content)
+            # print('Added group')
+
+        # Otherwise update the file if entry for user already present
+        else:
+            db.child('group').child(group_id).update({user_id : content})
+            # print('Updated group')
+
+    
 
     confirmation = "UwU bot-chan has successfully uploaded {}'s file...baka".format(update.message.from_user.username)
     context.bot.send_message(
         text=confirmation,
-        chat_id=group
+        chat_id=group_id
     )
 
     return ConversationHandler.END
@@ -171,12 +384,15 @@ def main():
     # Add commmand handler for help
     dp.add_handler(CommandHandler('help', help))
 
+    # Add command handler for clear
+    dp.add_handler(CommandHandler('clear', clear))
+
     # Add conversation handler for uploading document
     upload_conv_handler = ConversationHandler(
         entry_points=[CommandHandler('upload', upload)],
         states={
             1 : [MessageHandler(Filters.document.file_extension("ics"), on_doc_upload), CommandHandler('cancel', cancel_upload)],
-            2 : [MessageHandler(Filters.regex('(?i)^yes$|^no$'), ask_confirmation), MessageHandler(~Filters.regex('(?i)^yes$|^no$'), not_understood)]
+            2 : [MessageHandler(Filters.regex('(?i)^yes$|^no$'), ask_confirmation), MessageHandler(~Filters.regex('(?i)^yes$|^no$'), upload_not_understood)]
         },
         fallbacks=[MessageHandler(~Filters.document.file_extension("ics"), reprompt), CommandHandler('cancel', cancel_upload)]
     )
@@ -186,10 +402,11 @@ def main():
     query_conv_handler = ConversationHandler(
         entry_points=[CommandHandler('find', find)],
         states={
-            1 : [MessageHandler(Filters.text, find_persons_to_query), CommandHandler("cancel", cancel_find)],
-            2 : [MessageHandler(Filters.text, find_start_time), CommandHandler("cancel", cancel_find)],
-            3 : [MessageHandler(Filters.text, find_end_time), CommandHandler("cancel", cancel_find)],
-            4 : [MessageHandler(Filters.text, find_min_interval), CommandHandler("cancel", cancel_find)],
+            1 : [MessageHandler(Filters.regex('(?i)^Cancel$'), cancel_find), MessageHandler(Filters.text, find_persons_to_query)],
+            2 : [MessageHandler(Filters.text, find_start_time), MessageHandler(Filters.text, start_not_understood), CommandHandler("cancel", cancel_find)],
+            3 : [MessageHandler(Filters.text, find_end_time), MessageHandler(Filters.text, end_not_understood), CommandHandler("cancel", cancel_find)],
+            4 : [MessageHandler(Filters.text, find_min_interval), MessageHandler(Filters.text, interval_not_understood), CommandHandler("cancel", cancel_find)],
+            5 : [MessageHandler(Filters.text, display_result)]
         },
         fallbacks=[CommandHandler('cancel', cancel_find)]
     )
