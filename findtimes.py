4382228baaa76a72f5e2fe2978a1e4cb4e8dba85
Patrick <patricktan9955@hotmail.com>
2021-07-16 02:54:18 +0800

    Fixed various bugs in time-finding algorithm.
    1) Fixed some off-by-one errors in algorithm.
    2) Handled cases (when finding blocks of time that meet min interval) where free times may spill over to the next day.
    3) Fixed raising of ValueError when start datetime > end datetime. Error will now be correctly raised to the bot.
    4) Fixed rounding off of event end times.

--- findtimes.py
+++ findtimes.py
@@ -114,93 +114,6 @@ def new_parse_output_ics(cal_str, start_datetime, end_datetime):
     # print(sorted(event_list, key = lambda e : e[0]))
     return sorted(event_list, key = lambda e : e[0])
 
-# def parse_output_ics(in_filepath):
-    '''
-    Takes as input a filepath to a .ics file and returns a list of events in the ics file,
-    represented as lists of [start_datetime, end_datetime], sorted in ascending order
-    by start_datetime.
-
-    Future Changes: 
-    1) Currently does not work on recurring events
-    2) Currently does not deal with events where end_datetime does not exist
-    3) Change input to function to take in a string representation of an .ics file in line with 
-        future changes to merge_ics
-    4) Currently does not work on different timezones. (Consider using regex matching)
-
-    :param in_filepath: A string representing a filepath to the .ics file to be parsed.
-    :returns: A list of events, represented as a list of lists of [start_datetime, end_datetime], sorted in ascending order
-            by start_datetime.
-    '''
-    events_list = []
-    with open(in_filepath) as f:
-        # Temp container for start date
-        start_date_temp = None
-        
-        for line in f:
-            # When start datetime found
-            if line[0:7] == "DTSTART":
-                start_date = line.split(":")[1].strip()
-                
-                # Deal with UTC dates
-                if start_date[-1] == "Z":
-                    start_date = start_date[:-1]
-                
-                # Parse date
-                start_date_temp = datetime.strptime(start_date, "%Y%m%dT%H%M%S")
-
-            # When end datetime found
-            elif line[0:5] == "DTEND":
-                end_date = line.split(":")[1].strip()
-
-                # Deal with UTC dates
-                if end_date[-1] == "Z":
-                    end_date = end_date[:-1]
-
-                # Parse date
-                end_date = datetime.strptime(end_date, "%Y%m%dT%H%M%S")
-
-                # Append event to events_list
-                event = [copy.copy(start_date_temp), end_date]
-                events_list.append(event)
-
-                # Resets start_date_temp
-                start_date_temp = None
-    
-    return sorted(events_list, key = lambda e : e[0])
- 
-# def filter(events_list, start_date, end_date):
-    '''
-    Takes in a list of events represented as a list of lists of [start_datetime, end_datetime] and
-    filters out events that do not overlap with the interval [start_date, end_date] as prescribed
-    by the user. Operation performed using an interval tree.
-
-    Future Changes:
-    1) Currently does not handle overnight events properly in filtering, since events that spill over to 
-    the next day retain their original end date -> Causes KeyError in main function. Need to manually edit the 
-    start/end date during filtering so as to respect the provided start/end date by the user.
-
-    :param events_list: A list of events represented as a list of lists of [start_datetime, end_datetime]
-    :param start_date: A datetime object representing the start date of the desired search space.
-    :param end_date: A datetime object representing the end date of the desired search space.
-    :returns: A filtered list of events that overlap with the given [start_date, end_date] interval.
-    '''
-    interval_list = []
-    for event in events_list:
-        event_interval = Interval(event[0], event[1])
-        interval_list.append(event_interval)
-
-    tree = IntervalTree(interval_list)
-
-    filtered_raw = list(tree.overlap(start_date, end_date))
-    
-    filtered = []
-    for item in filtered_raw:
-        temp = [item.begin, item.end]
-        filtered.append(temp)
-    
-    # print(filtered)
-    return filtered
-
 def daterange(start_date, end_date):
     '''
     A function that allows for iteration of all dates between the provided start and end date.
@@ -232,11 +145,8 @@ def find_free_time(input_ics_strs, start_datetime, end_datetime, min_hourly_inte
     events = new_parse_output_ics(merge_ics(input_ics_strs) , start_datetime, end_datetime)
     # print(events)
     # Check for case where end_datetime less than start_datetime
-    try:
-        if end_datetime <= start_datetime:
-            raise ValueError
-    except ValueError:
-        print("Invalid interval provided!")
+    if end_datetime <= start_datetime:
+        raise ValueError
         
     start_date = start_datetime.date()
     end_date = end_datetime.date()
@@ -247,11 +157,16 @@ def find_free_time(input_ics_strs, start_datetime, end_datetime, min_hourly_inte
 
     for event in events:
         event_start_date = event[0].date()
-        event_end_date = event[1].date()
         event_start_time = event[0].time().hour
-        # cond = event[1].time().minute > 0 or event[1].time().second > 0 or event[1].time().microsecond > 0
-        # event_end_time = (event[1].time().hour + 1) if (cond) else event[1].time().hour
-        event_end_time = event[1].time().hour 
+        
+        cond = event[1].time().minute > 0 or event[1].time().second > 0 or event[1].time().microsecond > 0
+
+        # Round off event end time
+        if cond:
+            event[1] = event[1] + timedelta(hours=1)
+        
+        event_end_time = event[1].time().hour
+        event_end_date = event[1].date()
 
         # Case 1: Start and end date are the same
         if (event_start_date == event_end_date):
@@ -271,32 +186,61 @@ def find_free_time(input_ics_strs, start_datetime, end_datetime, min_hourly_inte
                 temp[i] = False
 
             temp = free_time_dict[event_end_date]
-            for i in range(0, event_end_time + 1):
+            for i in range(0, event_end_time):
                 temp[i] = False
 
+    # for k,v in free_time_dict.items():
+    #     print('{}:{}'.format(k,v))
     # print(free_time_dict)
     final_results = {}
 
     # Find intervals of min size provided
+    temp = None
+    spills_over = False
+    spill_interval = 0
+    spill_date = None
+
     for date, timetable in free_time_dict.items():
         results = []
         current_start = -1
         current_end = -1
         interval_cnt = 0
-        for i in range(23):
-            if timetable[i]:
-                if interval_cnt == 0:
-                    current_start = i
-                interval_cnt += 1
-            else:
-                if (interval_cnt >= min_hourly_interval):
-                    current_end = i
-                    results.append((current_start, current_end))
-                current_start = -1
-                current_end = -1
-                interval_cnt = 0
+        if not spills_over:
+            for i in range(24):
+                if timetable[i]:
+                    if interval_cnt == 0:
+                        current_start = i
+                    interval_cnt += 1
+                else:
+                    if (interval_cnt >= min_hourly_interval):
+                        current_end = i
+                        results.append((current_start, current_end))
+                    current_start = -1
+                    current_end = -1
+                    interval_cnt = 0
+        else:
+            for i in range(24):
+                if timetable[i]:
+                    if interval_cnt == 0:
+                        current_start = i
+                    interval_cnt += 1
+                else:
+                    if (interval_cnt + spill_interval >= min_hourly_interval):
+                        current_end = i
+                        final_results[spill_date].append(temp)
+                        results.append((current_start, current_end))
+                    temp = None
+                    spills_over = False
+                    spill_interval = 0
+                    current_start = -1
+                    current_end = -1
+                    interval_cnt = 0
+        
         if interval_cnt > 0 and current_end == -1:
-            results.append((current_start, 24))
+            temp = (current_start, 24)
+            spills_over = True
+            spill_interval = interval_cnt
+            spill_date = str(date)
 
         final_results[str(date)] = results
 
@@ -304,8 +248,8 @@ def find_free_time(input_ics_strs, start_datetime, end_datetime, min_hourly_inte
     # print(final_results)    
 
 if __name__ == '__main__':
-    start = datetime(2021, 5, 29, 0, 0, 0)
-    end = datetime(2021, 6, 3, 0, 0, 0)
+    start = datetime(2021, 3, 15, 0, 0, 0)
+    end = datetime(2021, 3, 21, 23, 0, 0)
     # print(new_parse_output_ics(fel_str, start, end))
     # print(find_free_time([fel_str], start, end, 5))
-    # print(find_free_time([FEL_STR, TEST_1_STR], start, end, 1))
+    # print('\n' + str(find_free_time([MALC_STR, FEL_STR], start, end, 14)))
